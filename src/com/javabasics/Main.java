f6672c8b506c6ef1eeb48919a6f3bf927e2d474b
freddykembo <freddykembo@gmail.com>
2023-01-11 19:03:01 +0300

    Creating variables

--- src/com/javabasics/Main.java
+++ src/com/javabasics/Main.java
@@ -1,7 +1,12 @@
 package com.javabasics;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int number = 100;
+        String name = "Freddy";
+        double amount = 250.50;
+        Date date = new Date();
     }
 }
