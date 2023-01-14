ead847f82fe64346da5d44e6292042062212c72c
freddykembo <freddykembo@gmail.com>
2023-01-14 19:06:20 +0300

    String class

--- src/com/javabasics/Main.java
+++ src/com/javabasics/Main.java
@@ -5,30 +5,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // Difference between Primitive and Reference types
-        int a = 10;
-        int b = a; // Copied a value to a different memory address
-        a = 100;
-        System.out.println("a = " + a + " - b = " + b);
+        // String
+        String name = "freddykembo";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(0));
 
-        // Pointing to the same reference
-        Person fred = new Person("Fred");
-        Person kembo = fred;
-
-        System.out.println("Before changing fred");
-        System.out.println(fred.name + " " + kembo.name);
-
-        fred.name = "Freddy";
-
-        System.out.println("After changing fred");
-        System.out.println(fred.name + " " + kembo.name);
-    }
-
-    static class Person {
-        String name;
-
-        Person(String name) {
-            this.name = name;
-        }
+        String secondName = "kembo";
+        System.out.println(name.contains(secondName));
+        System.out.println(name.equals(secondName));
     }
 }
