25ceb7145336b0866a1c4f628d881f7b579224b6
freddykembo <freddykembo@gmail.com>
2023-01-14 18:46:34 +0300

    Differences between primitive and reference types

--- src/com/javabasics/Main.java
+++ src/com/javabasics/Main.java
@@ -5,12 +5,30 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // Non-primitive data types AKA Reference types
-        String name = new String("freddykembo");
-        System.out.println(name.toUpperCase());
+        // Difference between Primitive and Reference types
+        int a = 10;
+        int b = a; // Copied a value to a different memory address
+        a = 100;
+        System.out.println("a = " + a + " - b = " + b);
 
-        LocalDate now = LocalDate.now();
-        System.out.println(now);
-        System.out.println(now.getMonth());
+        // Pointing to the same reference
+        Person fred = new Person("Fred");
+        Person kembo = fred;
+
+        System.out.println("Before changing fred");
+        System.out.println(fred.name + " " + kembo.name);
+
+        fred.name = "Freddy";
+
+        System.out.println("After changing fred");
+        System.out.println(fred.name + " " + kembo.name);
+    }
+
+    static class Person {
+        String name;
+
+        Person(String name) {
+            this.name = name;
+        }
     }
 }
