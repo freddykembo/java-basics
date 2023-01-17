a2f25559834fdd4dc7f6cd4b32d56b1694b3da2b
freddykembo <freddykembo@gmail.com>
2023-01-17 23:40:34 +0300

    Arrays

--- src/com/javabasics/Main.java
+++ src/com/javabasics/Main.java
@@ -1,27 +1,32 @@
 package com.javabasics;
 
 
+import java.lang.reflect.Array;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // Switch statement
-        String gender = "FEMALE";
+        // Arrays
 
-        switch (gender.toUpperCase()) {
-            case "FEMALE":
-                System.out.println("I am a Female");
-                break;
-            case "MALE":
-                System.out.println("I am a Male");
-                break;
-            case "PREFER NOT TO SAY":
-                System.out.println("PREFER NOT TO SAY");
-                break;
-            default:
-                System.out.println("Unknown gender");
-        }
+//        int [] numbers = new int[3];
+//        numbers[0] = 2;
+//        numbers[1] = 1;
+//        numbers[2] = 0;
+
+        int [] numbers = {0, 1, 2, 3}; // No size of array defined. Open to add
+        String [] names = {"freddy", "kembo"};
+
+        int one = numbers[1];
+        String lastName = names[names.length - 1];
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(names));
+        System.out.println(names.length);
+        System.out.println(one);
+        System.out.println(lastName);
     }
 }
