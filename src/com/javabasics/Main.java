e889b1b8d7df4888e4ccfe866d3a0e39a47db7b8
freddykembo <freddykembo@gmail.com>
2023-01-18 08:41:36 +0300

    Methhods. User defined method

--- src/com/javabasics/Main.java
+++ src/com/javabasics/Main.java
@@ -1,26 +1,27 @@
 package com.javabasics;
 
 
-import java.time.LocalDate;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Scanner
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String userName = scanner.nextLine();
-        System.out.println("Hello " + userName);
+        // Methods
+        char [] letters = {'A', 'A', 'B', 'C', 'D', 'D', 'D'};
 
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
-        int year = LocalDate.now().minusYears(age).getYear();
-        System.out.println("You were born in " + year);
+        int count = countOccurrences(letters, 'A');
+        System.out.println(count);
+    }
 
-        if (age >= 18) {
-            System.out.println("and you are an adult :)");
-        } else {
-            System.out.println("and you are not an adult :(");
+    public static int countOccurrences(char [] letters, char searchLetter) {
+//        System.out.println(Arrays.toString(letters));
+//        System.out.println(searchLetter);
+        int count = 0;
+
+        for (char letter : letters) {
+            if (letter == searchLetter) {
+                count++;
+            }
         }
+        return count;
     }
 }
